343c0749c6b63efb5b5e7be702c4fbcfb58b923c
xarvic <xarvix@web.de>
2021-04-27 12:27:29 +0200

    updated druid-widget-nursery version

--- src/clock.rs
+++ src/clock.rs
@@ -1,80 +1,65 @@
-use druid::{Widget, LifeCycle, EventCtx, PaintCtx, BoxConstraints, LifeCycleCtx, Size, LayoutCtx, Event, Env, UpdateCtx, RenderContext, Color, Point, Vec2};
-use druid::kurbo::{Shape, PathEl};
-use druid_widget_nursery::animation::{Animator, AnimationCurve, AnimationId, SimpleCurve, AnimationDirection};
+use druid::{Widget, LifeCycle, EventCtx, PaintCtx, BoxConstraints, LifeCycleCtx, Size, LayoutCtx, Event, Env, UpdateCtx, RenderContext, Color, Point};
+use druid::kurbo::PathEl;
+use druid::piet::{StrokeStyle, LineJoin};
+use druid_widget_nursery::animation::{SimpleCurve, Animated};
 use std::time::Duration;
 
 pub struct Clock {
-    animation_current: f64,
-    animation_duration: f64,
-    start_h1: f64,
-    start_h2: f64,
-    curr_h1: f64,
-    curr_h2: f64,
+    position: Animated<(f64, f64)>,
 }
 
 impl Clock {
     pub fn new() -> Self {
         Clock {
-            animation_current: 0.0,
-            animation_duration: 0.8,
-            start_h1: 0.0,
-            start_h2: 0.0,
-            curr_h1: 0.0,
-            curr_h2: 0.0
+            position: Animated::new(
+                (0.0, 0.0),
+                Duration::from_secs_f64(0.5),
+                SimpleCurve::EaseInOut,
+                false,
+            )
         }
     }
-    fn start_animation(&mut self) {
-        self.animation_current = 0.0;
+}
+
+impl Default for Clock {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
 impl Widget<(f64, f64)> for Clock {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut (f64, f64), _env: &Env) {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, _: &mut (f64, f64), _env: &Env) {
         if let Event::AnimFrame(nanos) = event {
-            self.animation_current += *nanos as f64 * 0.000000001 / self.animation_duration;
-            let mut curve = AnimationCurve::Simple(SimpleCurve::EaseInOut);
-            let progress = curve.translate(self.animation_current).min(1.);
-
-            self.curr_h1 = self.start_h1 + (data.0 - self.start_h1) * progress;
-            self.curr_h2 = self.start_h2 + (data.1 - self.start_h2) * progress;
-
-            ctx.request_paint();
-
-            if self.animation_current < 1.0 {
-                ctx.request_anim_frame();
-            }
+            self.position.update(*nanos, ctx);
         }
     }
 
     fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &(f64, f64), _env: &Env) {
         if let LifeCycle::WidgetAdded = event {
-            self.start_h1 = data.0;
-            self.curr_h1 = data.0;
-            self.start_h2 = data.1;
-            self.curr_h2 = data.1;
+            self.position.jump_to_value(*data);
         }
     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &(f64, f64), data: &(f64, f64), env: &Env) {
-        self.start_h1 = old_data.0;
-        self.start_h2 = old_data.1;
-        self.start_animation();
-        ctx.request_anim_frame();
+    fn update(&mut self, ctx: &mut UpdateCtx, _: &(f64, f64), data: &(f64, f64), _env: &Env) {
+        self.position.animate(*data);
+        if self.position.animating() {
+            ctx.request_anim_frame();
+        }
     }
 
-    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &(f64, f64), env: &Env) -> Size {
+    fn layout(&mut self, _ctx: &mut LayoutCtx, bc: &BoxConstraints, _data: &(f64, f64), _env: &Env) -> Size {
         bc.constrain_aspect_ratio(1.0, 50.0)
     }
 
-    fn paint(&mut self, ctx: &mut PaintCtx, data: &(f64, f64), env: &Env) {
+    fn paint(&mut self, ctx: &mut PaintCtx, _data: &(f64, f64), _env: &Env) {
         let back = ctx.size().to_rect().to_ellipse();
 
         ctx.fill(back, &Color::GRAY);
 
         let center = Point::new(ctx.size().width / 2.0, ctx.size().height / 2.0);
 
-        let vec0 = self.curr_h1.sin_cos();
-        let vec1 = self.curr_h2.sin_cos();
+        let vec0 = self.position.get().0.sin_cos();
+        let vec1 = self.position.get().1.sin_cos();
         let radius = ctx.size().width / 2.0 - 2.0;
 
         let path = [
@@ -83,6 +68,9 @@ impl Widget<(f64, f64)> for Clock {
             PathEl::LineTo(Point::new(center.x + vec1.1 * radius, center.y + vec1.0 * radius)),
         ];
 
-        ctx.stroke(&path as &[PathEl], &Color::grey8(0), 2.5);
+        let style = StrokeStyle::new()
+            .line_join(LineJoin::Round);
+
+        ctx.stroke_styled(&path as &[PathEl], &Color::grey8(0), 2.5, &style);
     }
 }
